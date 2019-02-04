7e7905b6953f96e4ba045d34ad0da195386f3492
Sergei Korchemkin <hellk.pub@gmail.com>
2019-02-04 06:27:44 +0300

    FIX: Don't play sound when starting the timer

--- app/src/main/java/net/xorium/roundtimerk/ButtonTimer.kt
+++ app/src/main/java/net/xorium/roundtimerk/ButtonTimer.kt
@@ -10,43 +10,48 @@ private const val TICK: Long = 10 // ms
 
 /* Turns a button into a button with timer */
 fun add(context: Context, b: Button, duration: Long) {
-    val bt = ButtonTimer(context, b, duration)
+    val buttonTimer = ButtonTimer(context, b, duration)
 
     b.setOnClickListener {
-        if (bt.isClicked)
-            bt.stop()
+        if (buttonTimer.isClicked)
+            buttonTimer.stop()
         else
-            bt.click()
+            buttonTimer.click()
     }
 }
 
-class ButtonTimer (context: Context, private val btn: Button, startTime: Long)
+class ButtonTimer (context: Context, private val button: Button, startTime: Long)
     : CountDownTimer(startTime, TICK) {
     var isClicked: Boolean = false
         private set
-    private val btnDefaultText: String = btn.text.toString()
+    private var soundEnabled: Boolean = false
+    private val buttonDefaultText: String = button.text.toString()
     private val time: Time = Time(startTime)
-    private val beep: MediaPlayer = MediaPlayer.create(context, R.raw.beep)
+    private val beepSound: MediaPlayer = MediaPlayer.create(context, R.raw.beep)
 
     fun click() {
         super.start()
-        beep.start()
+        if (soundEnabled)
+            beepSound.start()
         isClicked = true
     }
 
     fun stop() {
         super.cancel()
         isClicked = false
-        btn.text = btnDefaultText
+        soundEnabled = false
+        button.text = buttonDefaultText
     }
 
     override fun onFinish() {
+        soundEnabled = true
         this.click() // keep restarting until cancelled
     }
 
     override fun onTick(msUntilFinished: Long) {
         time.time = msUntilFinished
         val ms = msUntilFinished % 1000 / 100
-        btn.setText("${time.toString().substring(3)}.${ms.toString()}")
+        val timestr = time.toString().substring(3)
+        button.setText("$timestr.$ms")
     }
 }
